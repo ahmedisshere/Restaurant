8d57eb3ae6764aa4a6afcdcede4ef2901e9cc959
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-21 08:55:14 +0600

    populating data

---
+++ src/data/dishes.js
@@ -0,0 +1,191 @@
+const DISHES = [
+    {
+        id: 0,
+        name: 'Ginger Burger',
+        image: 'assets/images/0burger.jpg',
+        category: 'meal',
+        label: 'Hot',
+        price: '199',
+        description: 'A unique combination of Indian uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, vidalia onion, Guntur chillies and Buffalo paneer',
+        comments: [
+            {
+                id:0,
+                rating: 5,
+                comment: "Imagine all the eatables, Living in confusion",
+                author: "Felps",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:1,
+                rating: 4,
+                comment: "Sends Anyone to heaven,I wish I could get my mother-in-law to eat it",
+                author: "Cartman",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:2,
+                rating: 5,
+                comment: "Just eat it!",
+                author: "Markeloff",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:3,
+                rating: 5,
+                comment: "Ultimate! Reaching for the stars!",
+                author: "Robinhood",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:4,
+                rating: 5,
+                comment: "It's your birthday,we are gonna party!",
+                author: "Ikaras",
+                date: "2018-10-16T17:57:28.56094Z"
+            }
+        ]
+    },
+    {
+        
+            id: 1,
+            name: 'Chicken BBQ Pizza',
+            image: 'assets/images/0burger.jpg',
+            category: 'meal',
+            label: 'Hot',
+            price: '199',
+            description: 'Deep fried zucchini coated with mildly spiced chickpea flour batter accompained with a sweet-tangy tamarind sauce',
+            comments: [
+                {
+                    id:0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, Living in confusion",
+                    author: "Felps",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:1,
+                    rating: 4,
+                    comment: "Sends Anyone to heaven,I wish I could get my mother-in-law to eat it",
+                    author: "Cartman",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:2,
+                    rating: 5,
+                    comment: "Just eat it!",
+                    author: "Markelof",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:3,
+                    rating: 5,
+                    comment: "Ultimate! Reaching for the stars!",
+                    author: "Ikaras",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:4,
+                    rating: 5,
+                    comment: "It's your birthday,we are gonna party!",
+                    author: "Robinhood",
+                    date: "2018-10-16T17:57:28.56094Z"
+                }
+            ]
+       
+    },
+    {
+        id: 2,
+            name: 'French Fries',
+            image: 'assets/images/0burger.jpg',
+            category: 'appetizer',
+            label: 'Hot',
+            price: '199',
+            description: 'A quintissential confusion experience , it is a vada or is it a donut?',
+            comments: [
+                {
+                    id:0,
+                    rating: 5,
+                    comment: "Imagine all the eatables, Living in confusion",
+                    author: "Felps",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:1,
+                    rating: 4,
+                    comment: "Sends Anyone to heaven,I wish I could get my mother-in-law to eat it",
+                    author: "Cartman",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:2,
+                    rating: 5,
+                    comment: "Just eat it!",
+                    author: "Ikaras",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:3,
+                    rating: 5,
+                    comment: "Ultimate! Reaching for the stars!",
+                    author: "Markelof",
+                    date: "2018-10-16T17:57:28.56094Z"
+                },
+                {
+                    id:4,
+                    rating: 5,
+                    comment: "It's your birthday,we are gonna party!",
+                    author: "Robinhood",
+                    date: "2018-10-16T17:57:28.56094Z"
+                }
+            ]
+    },
+    {
+        id: 3,
+        name: 'Cheese Cake',
+        image: 'assets/images/0burger.jpg',
+        category: 'dessert',
+        label: 'Hot',
+        price: '199',
+        description: 'A delectable, semi-sweet New York Style Cheese Cake , with graham cracker crust and spiced with Indian cardamoms',
+        comments: [
+            {
+                id:0,
+                rating: 5,
+                comment: "Imagine all the eatables, Living in confusion",
+                author: "Felps",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:1,
+                rating: 4,
+                comment: "Sends Anyone to heaven,I wish I could get my mother-in-law to eat it",
+                author: "Robinhood",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:2,
+                rating: 5,
+                comment: "Just eat it!",
+                author: "Cartman",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:3,
+                rating: 5,
+                comment: "Ultimate! Reaching for the stars!",
+                author: "Markeloff",
+                date: "2018-10-16T17:57:28.56094Z"
+            },
+            {
+                id:4,
+                rating: 5,
+                comment: "It's your birthday,we are gonna party!",
+                author: "Ikaras",
+                date: "2018-10-16T17:57:28.56094Z"
+            }
+        ]
+    }
+]
+
+
+export default DISHES;
