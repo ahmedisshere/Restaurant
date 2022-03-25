ff2407ad1913991596b3ed5ce53e9db41aac9985
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-26 05:52:33 +0600

    menu

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Card, CardImg , CardImgOverlay, CardBody , CardTitle } from "reactstrap";
 
 
 const MenuItem = props => {
     console.log(props);
     return (
         <div>
-
+            <card>
+                <CardBody>
+                    <CardImg width="100%" alt={props.name} src={props.img}/>
+                    <CardImgOverlay>
+                        <CardTitle>{props.name}</CardTitle>
+                    </CardImgOverlay>
+                </CardBody>
+            </card>
         </div>
     );
 }
